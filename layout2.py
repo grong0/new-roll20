63feb5074fd4f262c505cc6fb32da0237364af70
Garrett59100 <garrett59100@gmail.com>
2024-09-08 23:06:23 -0400

    stats and proficiencies

--- layout2.py
+++ layout2.py
@@ -28,6 +28,48 @@ def get_asset(filename: str, group: str):
         return f.read()
 
 
+def color_to_variable(color_name: str) -> str:
+    match color_name:
+        case "primary":
+            return "oklch(var(--p))"
+        case "primary-content":
+            return "oklch(var(--pc))"
+        case "secondary":
+            return "oklch(var(--s))"
+        case "secondary-content":
+            return "oklch(var(--sc))"
+        case "accent":
+            return "oklch(var(--a))"
+        case "accent-content":
+            return "oklch(var(--ac))"
+        case "neutral":
+            return "oklch(var(--n))"
+        case "neutral-content":
+            return "oklch(var(--nc))"
+        case "base-100":
+            return "oklch(var(--b1))"
+        case "base-200":
+            return "oklch(var(--b2))"
+        case "base-300":
+            return "oklch(var(--b3))"
+        case "base-content":
+            return "oklch(var(--bc))"
+        case "info":
+            return "oklch(var(--in))"
+        case "success":
+            return "oklch(var(--inc))"
+        case "warning":
+            return "oklch(var(--wa))"
+        case "warning-content":
+            return "oklch(var(--wac))"
+        case "error":
+            return "oklch(var(--er))"
+        case "error-content":
+            return "oklch(var(--erc))"
+        case _:
+            return "oklch(var(--bc))"
+
+
 def get_class_color(class_name: str):
     match class_name.lower():
         case "artificer":
@@ -85,6 +127,9 @@ async def title_card():
 #         return f
 
 
+@app.get("/assets/")
+
+
 @app.get("/player/name", response_class=HTMLResponse)
 async def player_name():
     return "George"
@@ -98,6 +143,41 @@ async def player_race():
 @app.get("/player/level", response_class=HTMLResponse)
 async def player_level():
     return "13"
+
+
+@app.get("/player/hitpoints/max", response_class=HTMLResponse)
+async def player_hitpoints_max():
+    return "304"
+
+
+@app.get("/player/hitpoints/current", response_class=HTMLResponse)
+async def player_hitpoints_current():
+    return "250"
+
+
+@app.get("/player/hitpoints/percent", response_class=HTMLResponse)
+async def player_hitpoints_percent():
+    return str(round((250 / 304) * 100)) + "%"
+
+
+@app.get("/player/hitpoints/temp", response_class=HTMLResponse)
+async def player_hitpoints_temp():
+    return "12"
+
+
+@app.get("/player/armorclass", response_class=HTMLResponse)
+async def player_armorclass():
+    return "20"
+
+
+@app.get("/player/initiative", response_class=HTMLResponse)
+async def player_initiative():
+    return "4"
+
+
+@app.get("/player/speed/walking", response_class=HTMLResponse)
+async def player_speed_walking():
+    return "30"
 
 
 @app.get("/player/classes/{class_name}/level", response_class=HTMLResponse)
@@ -146,12 +226,47 @@ def player_attribute(ability: str):
 
 @app.get("/player/skills/{skill}/status", response_class=HTMLResponse)
 def player_skill_status(skill: str):
-    return compile_component("skill-status-proficient")  #
+    return compile_component("skill-status-proficient")
 
 
 @app.get("/player/skills/{skill}/bonus", response_class=HTMLResponse)
 def player_skill_bonus(skill: str):
     return "5"
+
+
+@app.get("/player/senses/perception", response_class=HTMLResponse)
+def player_senses_perception():
+    return "18"
+
+
+@app.get("/player/senses/investigation", response_class=HTMLResponse)
+def player_senses_perception():
+    return "18"
+
+
+@app.get("/player/senses/insight", response_class=HTMLResponse)
+def player_senses_perception():
+    return "18"
+
+
+@app.get("/player/proficiencies/armor", response_class=HTMLResponse)
+def player_proficiencies_armor():
+    return "None"
+
+
+@app.get("/player/proficiencies/weapons", response_class=HTMLResponse)
+def player_proficiencies_weapons():
+    return "Crossbow, Light, Dagger, Dart, Quarterstaff, Sling"
+
+
+@app.get("/player/proficiencies/tools", response_class=HTMLResponse)
+def player_proficiencies_tools():
+    return "Tinkerers' Tools, Artisan Tools"
+
+
+@app.get("/player/proficiencies/languages", response_class=HTMLResponse)
+def player_proficiencies_languages():
+    return "Abyssal, Celestial, Draconic"
 
 
 @app.get("/player/actions/action", response_class=HTMLResponse)
