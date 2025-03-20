fc56bac9e055c0a3da92420c737387add8881f1b
Garrett59100 <garrett59100@gmail.com>
2025-03-20 12:45:56 -0400

    actually finished ActionsDAO + implemented as_dict() for Action - fixed basic_rules

--- src/DAO/ActionsDAO.py
+++ src/DAO/ActionsDAO.py
@@ -19,20 +19,21 @@ class Action:
         self.source = object['source'] if 'source' in object.keys() else ""
         self.page = object['page'] if 'page' in object.keys() else -1
         self.srd = object['srd'] if 'srd' in object.keys() else False
-        self.basicRules = object['basicRules'] if 'basicRules' in object.keys() else ""
+        self.basic_rules = object['basicRules'] if 'basicRules' in object.keys() else False
         self.time = object['time'] if 'time' in object.keys() else Time(None)
         self.entries = object['entries'] if 'entries' in object.keys() else []
         self.see_also_action = object['seeAlsoAction'] if 'seeAlsoAction' in object.keys() else []
         self.from_variant = object['fromVariant'] if 'fromVariant' in object.keys() else ""
-    
+
     def as_dict(self) -> dict[str, Any]:
         """This is effectivaly read only as it creates a copy
 
         Returns:
             dict[str, Any]: the class as a dict
         """
-        
+
         copy = self.__dict__.copy()
+        copy['time'] = self.time.__dict__
         return copy
 
 class Actions:
@@ -41,18 +42,18 @@ class Actions:
     def __init__(self) -> None:
         with open("data/raw/actions.json") as f:
             obj = json.load(f)
-            
+
             self.actions = {}
             for action in obj['action']:
                 self.actions[action['name']] = Action(action)
-    
+
     def as_dict(self) -> dict[str, Any]:
         """This is effectivaly read only as it creates a copy
 
         Returns:
             dict[str, Any]: the class as a dict
         """
-        
+
         copy = self.__dict__.copy()
         for action_name in self.actions.keys():
             copy['actions'][action_name] = self.actions[action_name].as_dict()
