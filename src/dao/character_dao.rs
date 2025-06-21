e0b3150c67fd25efc9a117bdfba2d8271d7235a2
Garrett59100 <garrett59100@gmail.com>
2025-06-21 07:40:39 -0400

    updates and notes for character_dao

--- src/dao/character_dao.rs
+++ src/dao/character_dao.rs
@@ -2,20 +2,47 @@ use std::collections::HashMap;
 
 use serde_json::Value;
 
-use super::{backgrounds_dao::Background, common::{CharacterItem, CharacterTreasure, Details}, feats_dao::Feat, races_dao::Race, spells_dao::Spell};
+use super::{
+    backgrounds_dao::Background,
+    common::{Ability, CharacterItem, CharacterTreasure, Details},
+    feats_dao::Feat,
+    races_dao::Race,
+    skills_dao::Skill,
+    spells_dao::Spell,
+};
 
+pub enum SkillStatus {
+    UNTRAINED,
+    TRAINED,
+    EXPERTISE,
+}
+
+pub struct Senses {
+    perception: u8,
+    investigation: u8,
+    insight: u8,
+}
+
+/// Speed could be gotten as a function that takes in from different places because you could have items that give you more speed
 pub struct Character {
-	name: String,
-	xp: u64,
-	race: Race,
-	classes: HashMap<String, u8>,
-	background: Background,
-	details: Details,
-	weapon_proficiencies: Vec<String>,
-	armor_proficiencies: Vec<String>,
-	tool_proficiencies: Vec<String>,
-	feats: Vec<Feat>,
-	spells: Vec<Spell>,
-	items: Vec<CharacterItem>,
-	treasure: CharacterTreasure
+    name: String,
+    xp: u64,
+    race: Race,
+    classes: HashMap<String, u8>, // TODO: refine and confirm validity
+    background: Background,       // TODO: refine and confirm validity
+    details: Details,
+    weapon_proficiencies: Vec<String>, // TODO: refine and confirm validity
+    armor_proficiencies: Vec<String>,  // TODO: refine and confirm validity
+    tool_proficiencies: Vec<String>,   // TODO: refine and confirm validity
+    feats: Vec<Feat>,                  // TODO: refine and confirm validity
+    spells: Vec<Spell>,                // TODO: refine and confirm validity
+    items: Vec<CharacterItem>,
+    treasure: CharacterTreasure,
+    ability: Ability,                    // TODO: refine and confirm validity
+    skills: HashMap<Skill, SkillStatus>, // TODO: refine and confirm validity
+    current_hp: u64,
+    max_hp: u64,
+    armor_class: u32,
+    initiative: u8,
+    senses: Senses, // TODO: refine and confirm validity because it could be a function
 }
