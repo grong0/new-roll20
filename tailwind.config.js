1c9d6aa9cd10c725bde5e11519faf1e859ffe352
Garrett59100 <garrett59100@gmail.com>
2025-07-13 01:20:59 -0400

    changed some small stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/*.html", "./components/*.html", "./components/**/*.html"],
-	// theme: {
-	// 	extend: {
-	// 		fontFamily: {
-	// 			sans: ["Poppins"],
-	// 		},
-	// 	},
-	// },
 	plugins: [require("daisyui"), require("@tailwindcss/typography")],
 };
