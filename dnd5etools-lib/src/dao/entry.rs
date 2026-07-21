ad1acc3ca5ca4e56d13b7492db977860586f32ee
grong0 <garrett59100@gmail.com>
2026-07-21 05:34:20 -0400

    entry init

---
+++ dnd5etools-lib/src/dao/entry.rs
@@ -0,0 +1,87 @@
+pub struct EntrySection {}
+
+pub struct EntryEntries {}
+
+pub struct EntryQuote {}
+
+pub struct EntryInlineEntries {}
+
+pub struct EntryEntriesInlineEntries {}
+
+pub struct EntryOptions {}
+
+pub struct EntryTableGroup {}
+
+pub struct EntryTable {}
+
+pub struct EntryTableRow {}
+
+pub struct EntryTableCell {}
+
+pub struct EntryList {}
+
+pub struct EntryBonus {}
+
+pub struct EntryBonusSpeed {}
+
+pub struct EntryDice {}
+
+pub struct EntryAbilityDc {}
+
+pub struct EntryAbilityAttackMod {}
+
+pub struct AbilityGeneric {}
+
+pub struct EntryLink {}
+
+pub struct EntryOptFeature {}
+
+pub struct EntryInset {}
+
+pub struct EntryInsetReadaloud {}
+
+pub struct EntryVariant {}
+
+pub struct EntryVariantInner {}
+
+pub struct EntryVariantSub {}
+
+pub struct EntryItem {}
+
+pub struct EntryItemSub {}
+
+pub struct EntryItemSpell {}
+
+pub struct EntryImage {}
+
+pub struct EntryGallery {}
+
+pub struct EntryActions {}
+
+pub struct EntryAttack {}
+
+pub struct EntryStatblockInline {}
+
+pub struct EntryStatblock {}
+
+pub struct EntryRefClassFeature {}
+
+pub struct EntryRefSubclassFeature {}
+
+pub struct EntryRefOptionalfeature {}
+
+pub struct EntryRefFeat {}
+
+pub struct EntryHr {}
+
+pub struct EntrySpellcasting {}
+
+pub struct EntryFlowchart {}
+
+pub struct EntryFlowBlock {}
+
+pub struct EntryIngredient {}
+
+pub struct EntryWrapped {}
+
+// An entry can be any one of the above types including either a string or an integer.
